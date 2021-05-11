8b9260538adf8dd14ef1c7bacab782dc35d97ecf
zhangshuo00 <443494891@qq.com>
2021-05-11 15:48:43 +0000

    publish

---
+++ 12-range/index.js
@@ -0,0 +1,1 @@
+$(function(){var a=$("#range"),n=$("#age");n.html(a.val()),a.change(function(){n.html(a.val())})});
---
+++ 21-h5-validation/index.js
@@ -0,0 +1,1 @@
+$(function(){var l=$("#rectangle-width"),u=$("#rectangle-height");function c(e,t){return Math.round(e*Math.pow(10,t))/Math.pow(10,t)}$(".rectangle form").submit(function(e){e.preventDefault();var t=Number(l.val()),a=Number(u.val()),r=c(2*t+2*a,2),n=c(t*a,2);$("#rectangle-perimeter").val(r),$("#rectangle-area").val(n)})});
